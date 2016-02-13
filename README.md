b6d7263ddd9269769187de52744bac621717dc6e
Derek Tamsen <dtamsen@gmail.com>
2016-02-12 23:14:00 -0800

    finish book

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # go_book-main
+[The Little Go Book](http://openmymind.net/The-Little-Go-Book/)
--- main.go
+++ main.go
@@ -2,16 +2,39 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"math/rand"
 )
 
-type Add func(a int, b int) int
-
 func main() {
-	fmt.Println(process(func(a int, b int) int {
-		return a + b
-	}))
+	c := make(chan int)
+	for i := 0; i < 5; i++ {
+		worker := &Worker{id: i}
+		go worker.process(c)
+	}
+
+	for {
+		select {
+		case c <- rand.Int():
+		case t := <-time.After(time.Millisecond * 100):
+			fmt.Println("timed out at", t)
+		}
+		time.Sleep(time.Millisecond * 50)
+	}
 }
 
-func process(adder Add) int {
-	return adder(1, 2)
+type Worker struct {
+	id int
+}
+
+func (w *Worker) process(c chan int) {
+	for {
+		select {
+		case data := <-c:
+			fmt.Printf("worker %d got %d\n", w.id, data)
+		case <-time.After(time.Millisecond * 10):
+			fmt.Println("Break time")
+			time.Sleep(time.Second)
+		}
+	}
 }
